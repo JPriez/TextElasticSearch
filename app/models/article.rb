cfdb608162cbce5fd36c3f6984d80854485bf025
Oleg-Kononenko <kononenko@active-bridge.com>
2015-07-28 17:53:58 +0300

    Fix elasticsearch

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,28 +4,34 @@ class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
-  def self.search(query)
-  __elasticsearch__.search(
-    {
-      query: {
-        multi_match: {
-          query: query,
-          fields: ['title^10', 'text']
-        }
-      }
-    }
-  )
-  end
-
   settings index: { number_of_shards: 1 } do
     mappings dynamic: 'false' do
-      indexes :title, analyzer: 'english'
+      indexes :title, analyzer: 'english', index_options: 'offsets'
       indexes :text, analyzer: 'english'
     end
   end
 
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
 end
-Article.import # for auto sync model with elastic search
 
 # Delete the previous articles index in Elasticsearch
 Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
